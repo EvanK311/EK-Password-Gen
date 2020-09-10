eb4725b98b18bdc3887e1a493eb33e8fe12bdf7d
Evan K <EvanK311@gmail.com>
2020-09-10 15:21:12 -0700

    ACTUAL commit of todo list for coding process

--- script.js
+++ script.js
@@ -18,6 +18,20 @@ function writePassword() {
 // TODO: Create a Confirm to ask if user wants uppercase characters
 // TODO: Create a Confirm to ask if user wants numbers characters
 // TODO: Create a Confirm to ask if user wants special characters
+ 
+
+// TODO: Create conditional for master array that is fed by sub arrays for lowercase, uppercase, etc. using if statements.
+// TODO: Create variable that holds sub array choices(MasterArray).
+// TODO: Create variable that holds characters the user wants
+// TODO: Select the specific number of characters chosen randomly by user according to how long password length selected was.
+
+// can use Math.floor(Math.random()) to get random numbers. use For loop that occurs the length of # of characters selected by user.
+// * for{var i=0} ; (i<"user prompt" ; i++){}
+
+
+// TODO: Add validation to make sure at least 1 character is selected.
+// TODO: add validation to length for between 8-128 characters
+// TODO: after all prompts done, 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
