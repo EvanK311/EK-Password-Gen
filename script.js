841970e114ece99f4cd9a68941335eec5c9de141
Evan K <EvanK311@gmail.com>
2020-09-10 19:17:55 -0700

    prompt working with validation, added loops and adds to master array

--- script.js
+++ script.js
@@ -40,19 +40,60 @@ function writePassword() {
 // TODO: after all prompts done, 
 
 // Add event listener to generate button
+var uCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var ranNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var ranSpec = ["!", "@", "#", "$", "$", "%", "?"]
+var passArray = [pLength]
+var passMaster = []
 
+
+var finalPass = (writePassword) 
 
 generateBtn.addEventListener("click", writePassword) 
   
-var pLength = prompt("What length of password do you need?")
+
+
+var pLength = prompt("What length of password do you need? Enter a number between 8 and 128")
+if (pLength < 8 || pLength > 128) {
+    prompt("You must enter a number between 8 and 128")
+  }
+  console.log(pLength)
 
   
-var lCase = confirm("Would you like to include lowercase letters?")
-var uCase = confirm("Would you like to include uppercase letters?")
+  var l = confirm("Would you like to include lowercase letters?")
+if (l) {
+  for (i = 0; i < lCase.length; i++) {
+    passMaster.push(lCase[i])
+    // console.log(lCase)
+  }
+}
+ 
+  var u = confirm("Would you like to include uppercase letters?")
+  if (u) {
+      for (i = 0; i < uCase.length; i++) {
+        passMaster.push(uCase[i])
+        // console.log(uCase)
+      }
+    }
+    
 var passNum = confirm("Would you like to include numbers?")
+if (passNum) {
+  for (i = 0; i < ranNum.length; i++) {
+    passMaster.push(ranNum[i])
+    // console.log(ranNum)
+  }
+}
 var specChar = confirm("Would you like to include special characters?")
+if (specChar) {
+  for (i = 0; i < specChar.length; i++) {
+    passMaster.push(specChar[i])
+    // console.log(ranSpec)
+  }
+}
+ 
 
-
+console.log(passMaster)
 
 
 
