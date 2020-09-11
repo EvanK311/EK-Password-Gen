b709f48449446126b4ffb66b9703c1a25a7784cd
Evan K <EvanK311@gmail.com>
2020-09-10 22:16:15 -0700

    completed generator and working outputgit add .!

--- script.js
+++ script.js
@@ -43,58 +43,62 @@ function writePassword() {
 var uCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var lCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var ranNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var ranSpec = ["!", "@", "#", "$", "$", "%", "?"]
-var passArray = [pLength]
+var ranSpec = ["!", "@", "#", "$", "&", "%", "?","(",")"]
 var passMaster = []
+var lastPass = ""
 
 
-var finalPass = (writePassword) 
 
 generateBtn.addEventListener("click", writePassword) 
   
 
 
-var pLength = prompt("What length of password do you need? Enter a number between 8 and 128")
-if (pLength < 8 || pLength > 128) {
+function generatePassword() {
+  var pLength = prompt("What length of password do you need? Enter a number between 8 and 128")
+  if (pLength < 8 || pLength > 128) {
     prompt("You must enter a number between 8 and 128")
   }
   console.log(pLength)
 
   
   var l = confirm("Would you like to include lowercase letters?")
-if (l) {
-  for (i = 0; i < lCase.length; i++) {
-    passMaster.push(lCase[i])
-    // console.log(lCase)
+  if (l) {
+    for (i = 0; i < lCase.length; i++) {
+      passMaster.push(lCase[i])
+      // console.log(lCase)
+    }
   }
-}
  
   var u = confirm("Would you like to include uppercase letters?")
   if (u) {
-      for (i = 0; i < uCase.length; i++) {
-        passMaster.push(uCase[i])
-        // console.log(uCase)
-      }
+    for (i = 0; i < uCase.length; i++) {
+      passMaster.push(uCase[i])
+      // console.log(uCase)
     }
+  }
     
-var passNum = confirm("Would you like to include numbers?")
-if (passNum) {
-  for (i = 0; i < ranNum.length; i++) {
-    passMaster.push(ranNum[i])
-    // console.log(ranNum)
+  var passNum = confirm("Would you like to include numbers?")
+  if (passNum) {
+    for (i = 0; i < ranNum.length; i++) {
+      passMaster.push(ranNum[i])
+      // console.log(ranNum)
+    }
   }
-}
-var specChar = confirm("Would you like to include special characters?")
-if (specChar) {
-  for (i = 0; i < specChar.length; i++) {
-    passMaster.push(specChar[i])
-    // console.log(ranSpec)
+  var specChar = confirm("Would you like to include special characters?")
+  if (specChar) {
+    for (i = 0; i < specChar.length; i++) {
+      passMaster.push(specChar[i])
+      // console.log(ranSpec)
+    }
   }
+  console.log(passMaster)
+  
+  for(p = 0; p < pLength; p++) {
+    lastPass += passMaster[Math.floor(Math.random() * passMaster.length)];
+    
+    // console.log(lastPass)
+    // console.log(passMaster)
+  }
+  return lastPass
 }
- 
-
-console.log(passMaster)
-
-
-
   
