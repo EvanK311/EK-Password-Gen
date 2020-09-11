df388cf1c4ab1052f9d714fb1b6edfd370217c4c
Evan K <EvanK311@gmail.com>
2020-09-11 15:55:43 -0700

    final loop fix to restore password values to 0

--- script.js
+++ script.js
@@ -12,38 +12,15 @@ function writePassword() {
 
 
 
-// TODO: Create the generatePassword function. Returns final password and all code should live inside this function.
- 
-
-// TODO: Need prompt to ask length of password then save that to VAR
-
-// TODO: Create a Confirm to ask if user wants lowercase characters
-// TODO: Create a Confirm to ask if user wants uppercase characters
-// TODO: Create a Confirm to ask if user wants numbers characters
-// TODO: Create a Confirm to ask if user wants special characters
- 
-
-// TODO: Create conditional for master array that is fed by sub arrays for lowercase, uppercase, etc. using if statements.
-// TODO: Create variable that holds sub array choices(MasterArray).
-// TODO: Create variable that holds characters the user wants
-// TODO: Select the specific number of characters chosen randomly by user according to how long password length selected was.
-
-// can use Math.floor(Math.random()) to get random numbers. use For loop that occurs the length of # of characters selected by user.
-// * for(var i=0 ; i<"user prompt" ; i++){}
-
-// TODO: inside forloop, push each character into final array
-// TODO: create array variable to hold final password.
-
 
 // TODO: Add validation to make sure at least 1 character is selected.
-// TODO: add validation to length for between 8-128 characters
 // TODO: after all prompts done, 
 
 // Add event listener to generate button
 var uCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var lCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var ranNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var ranSpec = ["!", "@", "#", "$", "&", "%", "?","(",")"]
+var ranChar = ["!", "@", "#", "$", "&", "%", "?","(",")"]
 var passMaster = []
 var lastPass = ""
 
@@ -54,6 +31,8 @@ generateBtn.addEventListener("click", writePassword)
 
 
 function generatePassword() {
+  passMaster = []
+  lastPass = ""
   var pLength = prompt("What length of password do you need? Enter a number between 8 and 128")
   if (pLength < 8 || pLength > 128) {
     prompt("You must enter a number between 8 and 128")
@@ -86,10 +65,13 @@ function generatePassword() {
   }
   var specChar = confirm("Would you like to include special characters?")
   if (specChar) {
-    for (i = 0; i < specChar.length; i++) {
-      passMaster.push(specChar[i])
+    for (i = 0; i < ranChar.length; i++) {
+      passMaster.push(ranChar[i])
       // console.log(ranSpec)
     }
+  }
+  else if (l && u && passNum && specChar === False) {
+    prompt("You must make at least 1 selection")
   }
   console.log(passMaster)
   
